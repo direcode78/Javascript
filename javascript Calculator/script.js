6a206d40b8cfbde622c49a0e64ada2b36b4d64e7
direcode <direcode@gmail.com>
2015-05-16 19:58:18 -0500

    calculator operators working

--- javascript Calculator/script.js
+++ javascript Calculator/script.js
@@ -1,32 +1,33 @@
-	
-// Declaring globle var 
+
+// Declaring globle var
 	var display = document.getElementById('display');
 // End globle var
 
 function clicked(number){
     var but = document.getElementById('but');
-   // var display = document.getElementById('display');
     var butVal = number;
     display.value += butVal;
 }
 
 function operatorVal(value){
-//var display = document.getElementById('display');
+  var operatorCheck = display.value.charAt(display.value.length-1);
+	    if(operatorCheck == "+" || operatorCheck == "-" || operatorCheck == "*" || operatorCheck == "/" ){
+				display.value += "";
+			}else{
 	    display.value += value;
+		}
 }
 
 function calculate(){
-	//var display = document.getElementById('display');
 	var evalu = eval(display.value);
 	 if(display.value != '') {
-		display.value = evalu; 
+		display.value = evalu;
 	}else {
 		display.value = "";
 	}
 }
 
 function clearDisplay(){
-//var display = document.getElementById('display');
     display.value = '' ;
 
 }
